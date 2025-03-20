d812980ebe85e6c722b19182f9ea8db697089aee
NetheriteOfficial <108182503+NetheriteOfficial@users.noreply.github.com>
2025-03-20 19:06:23 +0100

    Update Program.cs

--- pVM Tests/Program.cs
+++ pVM Tests/Program.cs
@@ -3,23 +3,6 @@ using pVM;
 
 uint health = 32;
 Console.WriteLine("Current Health: " + health);
-/*VM vm = new VM(new byte[]
-{
-    (byte)Opcodes.Push,          0x01, 0, 0, 0,
-    (byte)Opcodes.Push,          0x05, 0, 0, 0,
-    (byte)Opcodes.Add,
-    (byte)Opcodes.DebugPrintNum, 0x01, 0, 0, 0,
-    (byte)Opcodes.Push,          0x48, 0, 0, 0,
-    (byte)Opcodes.Push,          0x65, 0, 0, 0,
-    (byte)Opcodes.Push,          0x6C, 0, 0, 0,
-    (byte)Opcodes.Push,          0x6C, 0, 0, 0,
-    (byte)Opcodes.Push,          0x6F, 0, 0, 0,
-    (byte)Opcodes.DebugPrint,    0x05, 0, 0, 0,
-    (byte)Opcodes.Move,          0x01, 0x0F, 0, 0, 0,
-    (byte)Opcodes.CallGameFunc,  0x01, 0, 0, 0,
-    (byte)Opcodes.CallGameFunc,  0x00, 0, 0, 0,
-    (byte)Opcodes.Halt,
-});*/
 
 VM vm = new("app.pbc");
 vm.RegisterGameFunc(0, () =>
